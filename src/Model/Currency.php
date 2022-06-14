3610692e64da9f89c1c27a552fe119ced861a421
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2022-06-14 11:04:01 +0200

    Moved currency helper function to the Currency model.

--- src/Model/Currency.php
+++ src/Model/Currency.php
@@ -3,10 +3,34 @@
 namespace Siak\Tontine\Model;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Builder;
 
 class Currency extends Model
 {
+    /**
+     * @var Currency
+     */
+    public static Currency $current;
+
+    /**
+     * Format an amount of money
+     *
+     * @param int $amount
+     * @param bool $hideSymbol
+     *
+     * @return string
+     */
+    public static function format(int $amount, bool $hideSymbol = false): string
+    {
+        $options = self::$current->options;
+        $amount = number_format($amount, $options->precision,
+            $options->separator->decimal, $options->separator->thousand);
+        if($hideSymbol)
+        {
+            return $amount;
+        }
+        return $options->symbol->swap ? $options->symbol->value . $amount : $amount . ' ' . $options->symbol->value;
+    }
+
     /**
      * Indicates if the model should be timestamped.
      *
--- src/Model/Traits/HasCurrency.php
+++ src/Model/Traits/HasCurrency.php
@@ -4,36 +4,10 @@ namespace Siak\Tontine\Model\Traits;
 
 use Siak\Tontine\Model\Currency;
 
-use function number_format;
 use function intval;
 
 trait HasCurrency
 {
-    /**
-     * @var Currency
-     */
-    public static Currency $currency;
-
-    /**
-     * Format an amount of money
-     *
-     * @param int $amount
-     * @param bool $hideSymbol
-     *
-     * @return string
-     */
-    public static function format(int $amount, bool $hideSymbol = false): string
-    {
-        $options = self::$currency->options;
-        $amount = number_format($amount, $options->precision,
-            $options->separator->decimal, $options->separator->thousand);
-        if($hideSymbol)
-        {
-            return $amount;
-        }
-        return $options->symbol->swap ? $options->symbol->value . $amount : $amount . ' ' . $options->symbol->value;
-    }
-
     /**
      * Format an attribute value
      *
@@ -44,6 +18,6 @@ trait HasCurrency
      */
     public function money(string $attr, bool $hideSymbol = false): string
     {
-        return self::format(intval($this->$attr), $hideSymbol);
+        return Currency::format(intval($this->$attr), $hideSymbol);
     }
 }
