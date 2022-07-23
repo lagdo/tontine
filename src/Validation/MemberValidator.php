939390457fa9bf7e8c359f92ab91be91267bd577
Thierry Feuzeu <thierry.feuzeu@laplateforme.com>
2022-07-23 08:39:35 +0200

    Improved member phone number validation.

--- src/Validation/MemberValidator.php
+++ src/Validation/MemberValidator.php
@@ -3,10 +3,26 @@
 namespace Siak\Tontine\Validation;
 
 use Illuminate\Support\Facades\Validator;
+use Siak\Tontine\Service\TenantService;
 use Siak\Tontine\Validation\AbstractValidator;
+
+use function strtoupper;
 
 class MemberValidator extends AbstractValidator
 {
+    /**
+     * @var TenantService
+     */
+    private $tenantService;
+
+    /**
+     * @param TenantService $tenantService
+     */
+    public function __construct(TenantService $tenantService)
+    {
+        $this->tenantService = $tenantService;
+    }
+
     /**
      * @param array $values
      *
@@ -14,11 +30,12 @@ class MemberValidator extends AbstractValidator
      */
     public function validateItem(array $values): array
     {
+        $country = strtoupper($this->tenantService->tontine()->country->code);
         $validator = Validator::make($values, [
             'gender' => 'required|in:F,M',
             'name' => 'required|string|min:1',
             'email' => 'sometimes|required|email',
-            'phone' => 'sometimes|required|phone:AUTO',
+            'phone' => 'sometimes|required|phone:AUTO,' . $country,
             'birthday' => 'sometimes|required|date_format:Y-m-d',
             'city' => 'sometimes|required|string',
             'address' => 'sometimes|required|string',
