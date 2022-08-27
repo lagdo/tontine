8e5a533504ee2d507e61083851b1c7d8c301213b
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2022-08-27 08:42:09 +0200

    Fix translations.

--- resources/views/pages/charge/add.blade.php
+++ resources/views/pages/charge/add.blade.php
@@ -1,7 +1,7 @@
           <div class="section-body">
             <div class="row align-items-center">
               <div class="col-sm-8">
-                <h2 class="section-title">{{ __('tontine.charge.labels.add') }}</h2>
+                <h2 class="section-title">{{ __('tontine.charge.titles.add') }}</h2>
               </div>
               <div class="col-sm-4">
                 <div class="btn-group float-right" role="group" aria-label="">
