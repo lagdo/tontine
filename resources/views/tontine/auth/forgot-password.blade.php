a752ed6fd98cc096602d45122d99adafa96e396a
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2023-07-29 22:35:31 +0200

    Translated the auth pages.

--- resources/views/tontine/auth/forgot-password.blade.php
+++ resources/views/tontine/auth/forgot-password.blade.php
@@ -1,32 +1,30 @@
 @extends('tontine.auth.layout')
 
-@section('page-title', 'Forgot Password')
+@section('page-title', __('Reset Password'))
 
 @section('content-class', 'col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4')
 
 @section('content')
             <div class="card card-primary">
-              <div class="card-header"><h4>Forgot Password</h4></div>
+              <div class="card-header"><h4>{{ __('Reset Password') }}</h4></div>
 
               <div class="card-body">
-                <p class="text-muted">We will send a link to reset your password</p>
+                <p class="text-muted">{{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}</p>
                 <form method="POST" action="{{ route('password.email') }}">
                   @csrf
 
                   <div class="form-group">
-                    <label for="email">Email</label>
+                    <label for="email">{{ __('Email') }}</label>
                     <input id="email" type="email" class="form-control" name="email" tabindex="1" required autofocus>
                   </div>
 
                   <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
-                      Forgot Password
-                    </button>
+                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">{{ __('Email Password Reset Link') }}</button>
                   </div>
                 </form>
               </div>
             </div>
             <div class="mt-5 text-muted text-center">
-              <a href="{{ route('login') }}">Back to login</a>
+              <a href="{{ route('login') }}">{{ __('Back to the login page') }}</a>
             </div>
 @endsection
