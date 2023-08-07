cc3dd415158b0c447e1bbab6736b1d85530e13bb
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2023-08-07 06:03:02 +0200

    Change in libre tontines.
    - Multiple subscriptions allowed per member.

--- resources/views/tontine/pages/planning/subscription/page.blade.php
+++ resources/views/tontine/pages/planning/subscription/page.blade.php
@@ -2,26 +2,16 @@
                 <table class="table table-bordered">
                    <thead>
                     <tr>
-@if ($tontine->is_libre)
-                      <th style="padding-left: 10px">{{ $count }}</th>
-@else
-                      <th style="text-align: center">{{ $count }}</th>
-@endif
                       <th>{!! __('common.labels.name') !!}</th>
+                      <th style="text-align: center">{{ $count }}</th>
                     </tr>
                   </thead>
                  <tbody>
 @foreach ($members as $member)
                     <tr>
-                      <td class="table-member-subscription" data-member-id="{{ $member->id }}">
-@if ($tontine->is_libre)
-@if ($member->subscriptions_count > 0)
-                        <a href="javascript:void(0)" class="btn-subscription-del"><i class="fa fa-toggle-on"></i></a>
-@else
-                        <a href="javascript:void(0)" class="btn-subscription-add"><i class="fa fa-toggle-off"></i></a>
-@endif
-@else
-                        <div class="input-group float-right" style="width:auto;">
+                      <td>{{ $member->name }}</td>
+                      <td class="table-member-subscription">
+                        <div class="input-group float-right" data-member-id="{{ $member->id }}" style="width:auto;">
                           <div class="input-group-prepend">
                             <button type="button" class="btn btn-primary btn-sm btn-subscription-del"><i class="fas fa-minus"></i></button>
                           </div>
@@ -30,9 +20,7 @@
                             <button type="button" class="btn btn-primary btn-sm btn-subscription-add"><i class="fas fa-plus"></i></button>
                           </div>
                         </div>
-@endif
                       </td>
-                      <td>{{ $member->name }}</td>
                     </tr>
 @endforeach
                   </tbody>
