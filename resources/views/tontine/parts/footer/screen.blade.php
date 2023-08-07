0050021e54b50ccea55756de82889b71b75742da
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2023-08-07 08:30:20 +0200

    Lock screen in landscape on mobile devices.

---
+++ resources/views/tontine/parts/footer/screen.blade.php
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+  jaxon.dom.ready(function() {
+    // Check if it is a mobile device
+    if('ontouchstart' in document.documentElement)
+    {
+      // Lock screen in landscape mode
+      screen.orientation.lock('landscape');
+    }
+  });
+</script>
