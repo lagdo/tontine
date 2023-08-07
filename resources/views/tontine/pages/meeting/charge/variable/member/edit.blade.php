f80d5090e079c970437c87c77acf1408638e8c20
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2023-08-07 07:31:51 +0200

    Mak input borders more visible.

--- resources/views/tontine/pages/meeting/charge/variable/member/edit.blade.php
+++ resources/views/tontine/pages/meeting/charge/variable/member/edit.blade.php
@@ -1,5 +1,5 @@
                         <div class="input-group">
-                          {!! Form::text('amount', $amount, ['class' => 'form-control', 'style' => 'width:50px']) !!}
+                          {!! Form::text('amount', $amount, ['class' => 'form-control', 'style' => 'width:50px;border-color:#a1a1a1;']) !!}
                           <div class="input-group-append" data-member-id="{{ $id }}">
                             <button type="button" class="btn btn-primary btn-save-bill"><i class="fa fa-save"></i></button>
                           </div>
