1783563d580606628a7a1356250048137b00e46c
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2023-12-31 04:29:40 +0100

    Fix in migration.

--- database/migrations/2023_12_24_228124_create_pool_views.php
+++ database/migrations/2023_12_24_228124_create_pool_views.php
@@ -21,13 +21,13 @@ create view v_pools as select pp.id, rp.tontine_id,
             then (select sa.start_at from pool_rounds prb inner join sessions sa
                 on (prb.start_session_id = sa.id and prb.pool_id = pp.id))
             else (select min(start_at) from sessions sb where sb.round_id = pp.round_id)
-    end as 'start_at',
+    end as `start_at`,
     case
         when exists(select 1 from pool_rounds prc where prc.pool_id = pp.id)
             then (select sc.start_at from pool_rounds prd inner join sessions sc
                 on (prd.end_session_id = sc.id and prd.pool_id = pp.id))
             else (select max(start_at) from sessions sd where sd.round_id = pp.round_id)
-    end as 'end_at'
+    end as `end_at`
 from pools pp inner join rounds rp on pp.round_id = rp.id;
 SQL;
         DB::statement($sql);
@@ -45,7 +45,7 @@ create view v_pool_counters as select pp.id, pp.title, pp.amount,
                     (select sc.start_at from pool_rounds prc, sessions sc
                         where prc.pool_id = pp.id and prc.end_session_id  = sc.id))
             else (select count(*) from sessions sd where sd.round_id = pp.round_id)
-    end as 'sessions',
+    end as `sessions`,
     case
         when exists(select 1 from pool_rounds prd where prd.pool_id = pp.id)
             then (select count(*) from pool_session_disabled psa inner join sessions se
@@ -56,7 +56,7 @@ create view v_pool_counters as select pp.id, pp.title, pp.amount,
                     (select sg.start_at from pool_rounds prf, sessions sg
                         where prf.pool_id = pp.id and prf.end_session_id  = sg.id))
             else (select count(*) from pool_session_disabled psb where psb.pool_id = pp.id)
-    end as 'disabled_sessions'
+    end as `disabled_sessions`
 from pools pp inner join rounds rp on pp.round_id = rp.id;
 SQL;
         DB::statement($sql);
