1fcb215ae758201ea27510f63bee9dfd27050aab
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-01-04 01:26:57 +0100

    Added line breaks in feedback mail body.

--- resources/views/tontine/emails/feedbacks/received.blade.php
+++ resources/views/tontine/emails/feedbacks/received.blade.php
@@ -1,9 +1,9 @@
 Bonjour,
 
 Vous avez reçu un feedback @if ($user !== null)de l'utilisateur {{ $user->name }}@endif sur l'application Tontine.
-
+<br/><br/>
 Type: {{ $feedback->type }}
-
+<br/><br/>
 Message:
-
+<br/><br/>
 {{ $feedback->message }}
