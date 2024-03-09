6f4f39f593c654f0bf4e3d23ef66b1f3b63c62cd
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-03-09 02:52:08 +0100

    Fixed width for menu icons.

--- resources/views/tontine/app/default/parts/sidebar/menu.blade.php
+++ resources/views/tontine/app/default/parts/sidebar/menu.blade.php
@@ -10,10 +10,10 @@
           <ul class="sidebar-menu">
             <li class="menu-header">{{ __('tontine.menus.tontines') }}</li>
             <li><a class="nav-link" id="tontine-menu-tontines" href="{{ $ajax ? 'javascript:void(0)' : route('tontine.home') }}">
-              <i class="fa fa-user-shield"></i> <span>{{ __('tontine.menus.tontines') }}</span>
+              <i class="fa fa-fw fa-user-shield"></i> <span>{{ __('tontine.menus.tontines') }}</span>
             </a></li>
             <li><a class="nav-link" id="tontine-menu-users" href="javascript:void(0)">
-              <i class="fa fa-user-friends"></i> <span>{{ __('tontine.menus.users') }}</span>
+              <i class="fa fa-fw fa-user-friends"></i> <span>{{ __('tontine.menus.users') }}</span>
             </a></li>
           </ul>
           <div id="sidebar-menu-tontine">
--- resources/views/tontine/app/default/parts/sidebar/tontine.blade.php
+++ resources/views/tontine/app/default/parts/sidebar/tontine.blade.php
@@ -1,14 +1,14 @@
             <ul class="sidebar-menu">
               <li class="menu-header">{{ __('tontine.menus.tontine') }}</li>
               <li><a class="nav-link" id="tontine-menu-members" href="javascript:void(0)">
-                <i class="fa fa-users"></i> <span>{{ __('tontine.menus.members') }}</span>
+                <i class="fa fa-fw fa-users"></i> <span>{{ __('tontine.menus.members') }}</span>
               </a></li>
               <li><a class="nav-link" id="tontine-menu-categories" href="javascript:void(0)">
-                <i class="fa fa-cogs"></i> <span>{{ __('tontine.menus.categories') }}</span>
+                <i class="fa fa-fw fa-cogs"></i> <span>{{ __('tontine.menus.categories') }}</span>
               </a></li>
 
               <li class="menu-header">{{ __('tontine.menus.planning') }}</li>
               <li><a class="nav-link" id="planning-menu-sessions" href="javascript:void(0)">
-                <i class="fa fa-calendar-day"></i> <span>{{ __('tontine.menus.sessions') }}</span>
+                <i class="fa fa-fw fa-calendar-day"></i> <span>{{ __('tontine.menus.sessions') }}</span>
               </a></li>
             </ul>
