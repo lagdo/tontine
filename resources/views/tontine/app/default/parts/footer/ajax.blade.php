a1f48feda323935e814da2a8fcfc3430e785a65d
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-04-25 21:52:16 +0200

    Moved inline CSS and Js to static files.

--- resources/views/tontine/app/default/parts/footer/ajax.blade.php
+++ resources/views/tontine/app/default/parts/footer/ajax.blade.php
@@ -5,60 +5,21 @@
 {!! $jaxonCss !!}
 
 <script type='text/javascript'>
-  const tontine = {
-    home: () => {!! $jxnTontine->home() !!},
-    users: () => {!! $jxnInvite->home() !!},
-    labels: {
-      amount: "{{ __('common.labels.amount') }}",
-      percentage: "{{ __('meeting.loan.labels.percentage') }}",
-    },
-    titles: {
-      message: "{{ __('common.titles.message') }}",
-    },
-    messages: {
-      orientation: "{{ __('tontine.messages.screen.orientation') }}",
-    },
-  };
-  function showBalanceAmounts() {
-    {!! $jxnSession->showBalanceAmounts() !!};
-  }
-  function showBalanceAmountsWithDelay() {
-    setTimeout(() => {!! $jxnSession->showBalanceAmounts() !!}, 5);
-  }
-
-  function makeTableResponsive(tableId)
-  {
-    const table = document.querySelector('#' + tableId);
-    if(!table) {
-      return;
+    const tontine = {
+        home: () => {!! $jxnTontine->home() !!},
+        users: () => {!! $jxnInvite->home() !!},
+        labels: {
+            amount: "{{ __('common.labels.amount') }}",
+            percentage: "{{ __('meeting.loan.labels.percentage') }}",
+        },
+        titles: {
+            message: "{{ __('common.titles.message') }}",
+        },
+    };
+    function showBalanceAmounts() {
+        {!! $jxnSession->showBalanceAmounts() !!};
     }
-    const labels = Array.from(table.querySelectorAll('th')).map(th => th.innerText);
-    table.querySelectorAll('td')
-      .forEach((td, i) => td.setAttribute('data-label', labels[i % labels.length]));
-  }
-
-  function showSmScreen(targetId, wrapperId)
-  {
-    $('.sm-screen', $('#' + wrapperId)).removeClass('sm-screen-active');
-    $('#' + targetId).addClass('sm-screen-active');
-  }
-
-  function setSmScreenHandler(btnWrapperId, screensWrapperId = 'content-home')
-  {
-    const btnWrapper = $('#' + btnWrapperId);
-    $('button', btnWrapper).click(function() {
-      const targetId = $(this).attr('data-target');
-      if(!targetId) {
-        return;
-      }
-      // Show the target screen.
-      showSmScreen(targetId, screensWrapperId);
-      // Activate the button the user has clicked on.
-      $('button', btnWrapper).removeClass('btn-primary');
-      $('button', btnWrapper).removeClass('btn-outline-primary');
-      $('button', btnWrapper).addClass('btn-outline-primary');
-      $(this).removeClass('btn-outline-primary');
-      $(this).addClass('btn-primary');
-    });
-  }
+    function showBalanceAmountsWithDelay() {
+        setTimeout(() => {!! $jxnSession->showBalanceAmounts() !!}, 5);
+    }
 </script>
