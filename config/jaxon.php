15acc4d8e917ad5a85ef0286b784a91cde5bbde3
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-01-10 04:49:14 +0100

    Updated jaxon packages.

--- config/jaxon.php
+++ config/jaxon.php
@@ -34,6 +34,36 @@ return [
         ],
         'packages' => [
         ],
+        'dialogs' => [
+            'default' => [
+                'modal' => 'bootbox',
+                'alert' => 'toastr',
+                'confirm' => 'noty',
+            ],
+            // 'assets' => [
+            //     'include' => [
+            //         'all' => true,
+            //     ],
+            // ],
+            'bootbox' => [
+                'assets' => [
+                    'js' => "https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/6.0.0/bootbox.min.js",
+                ],
+            ],
+            'toastr' => [
+                'assets' => [
+                    'js' => false,
+                    'css' => false,
+                ],
+                'options' => [
+                    'alert' => [
+                        'closeButton' => true,
+                        'closeDuration' => 0,
+                        'positionClass' => 'toast-top-center',
+                    ],
+                ],
+            ],
+        ],
     ],
     'lib' => [
         'core' => [
@@ -61,37 +91,9 @@ return [
             'app' => [
                 'uri' => env('JAXON_JS_APP_URI', '/jaxon'),
                 'dir' => env('JAXON_JS_APP_DIR', public_path('/jaxon')),
-                'file' => env('JAXON_JS_APP_FILE', 'js-4.0.0-18'),
+                'file' => env('JAXON_JS_APP_FILE', 'js-4.0.0-19'),
                 'export' => env('JAXON_JS_APP_EXPORT', !env('APP_DEBUG')),
                 'minify' => env('JAXON_JS_APP_MINIFY', !env('APP_DEBUG')),
-            ],
-        ],
-        // 'assets' => [
-        //     'include' => [
-        //         'all' => true,
-        //     ],
-        // ],
-        'dialogs' => [
-            'default' => [
-                'modal' => 'bootbox',
-                'message' => 'toastr',
-                'question' => 'noty',
-            ],
-            'bootbox' => [
-                'assets' => [
-                    'js' => "https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/6.0.0/bootbox.min.js",
-                ],
-            ],
-            'toastr' => [
-                'assets' => [
-                    'js' => false,
-                    'css' => false,
-                ],
-                'options' => [
-                    'closeButton' => true,
-                    'closeDuration' => 0,
-                    'positionClass' => 'toast-top-center',
-                ],
             ],
         ],
     ],
