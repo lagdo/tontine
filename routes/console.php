a31cdaae3424b84d0eb75b16827dad4e1b1b61ed
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-04-12 19:19:09 +0200

    Delete obsolete commands.

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,8 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('annotations:mkdir', function () {
+    $path = storage_path('annotations');
+    file_exists($path) || mkdir($path, 0755, false);
+})->purpose('Create the cache dir for the Jaxon classes annotations');
