29ba4020785f919c685874fa94efbcd89420b9a9
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-07-25 10:27:27 +0200

    Change sidebar menu entries positions.

--- resources/views/tontine/app/default/parts/sidebar/admin.blade.php
+++ resources/views/tontine/app/default/parts/sidebar/admin.blade.php
@@ -6,6 +6,16 @@
                 <i class="fa fa-fw fa-house-user"></i> <span>{{ __('tontine.menus.guilds') }}</span>
               </a></li>
             </ul>
+            <ul class="sidebar-menu" id="guild-menu">
+              <li class="menu-header">{{ __('tontine.menus.guild') }}</li>
+
+              <li><a class="nav-link" id="guild-menu-members" role="link" tabindex="0">
+                <i class="fa fa-fw fa-users"></i> <span>{{ __('tontine.menus.members') }}</span>
+              </a></li>
+              <li><a class="nav-link" id="guild-menu-calendar" role="link" tabindex="0">
+                <i class="fa fa-fw fa-calendar-day"></i> <span>{{ __('tontine.menus.calendar') }}</span>
+              </a></li>
+            </ul>
             <ul class="sidebar-menu" id="finance-menu">
               <li class="menu-header">{{ __('tontine.menus.finance') }}</li>
 
@@ -17,15 +27,5 @@
               </a></li>
               <li><a class="nav-link" id="finance-menu-accounts" role="link" tabindex="0">
                 <i class="fa fa-fw fa-wallet"></i> <span>{{ __('tontine.menus.accounts') }}</span>
-              </a></li>
-            </ul>
-            <ul class="sidebar-menu" id="guild-menu">
-              <li class="menu-header">{{ __('tontine.menus.guild') }}</li>
-
-              <li><a class="nav-link" id="guild-menu-members" role="link" tabindex="0">
-                <i class="fa fa-fw fa-users"></i> <span>{{ __('tontine.menus.members') }}</span>
-              </a></li>
-              <li><a class="nav-link" id="guild-menu-calendar" role="link" tabindex="0">
-                <i class="fa fa-fw fa-calendar-day"></i> <span>{{ __('tontine.menus.calendar') }}</span>
               </a></li>
             </ul>
