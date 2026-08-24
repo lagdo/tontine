9e3d1eff15cbbad942b943716e72bd52d52f0992
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2026-08-24 18:39:33 +0200

    Update the migrations for Laravel 13.

--- config/session.php
+++ config/session.php
@@ -86,7 +86,7 @@ return [
     |
     */
 
-    'table' => env('SESSION_TABLE', 'sessions'),
+    'table' => env('SESSION_TABLE', 'user_sessions'),
 
     /*
     |--------------------------------------------------------------------------
